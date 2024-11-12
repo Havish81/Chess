f040d8349f850a86256817f7ee1af9f80dcbf01c
Arseniy Iakhymets <dwa37@txstate.edu>
2024-11-11 23:44:16 -0600

    Generated javadoc.

--- Chess.java
+++ Chess.java
@@ -1,17 +1,27 @@
-import board.GUIChessBoard;
-import piece.ChessPiece;
-import piece.PieceColor;
-import piece.Rook;
-import piece.Knight;
-import piece.Bishop;
-import piece.Queen;
-import piece.King;
-import piece.Pawn;
 
+import board.GUIChessBoard;
+import piece.Bishop;
+import piece.ChessPiece;
+import piece.King;
+import piece.Knight;
+import piece.Pawn;
+import piece.PieceColor;
+import piece.Queen;
+import piece.Rook;
+
+/**
+ * The `Chess` class represents a chess game with a board, pieces, and methods
+ * for initializing the board, moving pieces, and checking for valid moves.
+ */
 public class Chess {
+
     private ChessPiece[][] board;
     private GUIChessBoard guiBoard;
 
+    /**
+     * The constructor initializes a chess game by creating a chess board,
+     * setting up pieces, and displaying the board on a GUI interface.
+     */
     public Chess() {
         board = new ChessPiece[8][8];
         guiBoard = new GUIChessBoard(this::getPiece, this::movePiece); // Pass methods for piece access and movement
@@ -29,7 +39,9 @@ public class Chess {
         board[0][5] = new Bishop(PieceColor.Black);
         board[0][6] = new Knight(PieceColor.Black);
         board[0][7] = new Rook(PieceColor.Black);
-        for (int i = 0; i < 8; i++) board[1][i] = new Pawn(PieceColor.Black);
+        for (int i = 0; i < 8; i++) {
+            board[1][i] = new Pawn(PieceColor.Black);
+        }
 
         board[7][0] = new Rook(PieceColor.White);
         board[7][1] = new Knight(PieceColor.White);
@@ -39,19 +51,43 @@ public class Chess {
         board[7][5] = new Bishop(PieceColor.White);
         board[7][6] = new Knight(PieceColor.White);
         board[7][7] = new Rook(PieceColor.White);
-        for (int i = 0; i < 8; i++) board[6][i] = new Pawn(PieceColor.White);
+        for (int i = 0; i < 8; i++) {
+            board[6][i] = new Pawn(PieceColor.White);
+        }
     }
 
+    /**
+     * The method returns the ChessPiece located at the specified row and column
+     * on the board.
+     *
+     * @param row The `row` parameter represents the row number of the chess
+     * board where the piece is located.
+     * @param col The `col` parameter represents the column number in a chess
+     * board where you want to retrieve a chess piece.
+     * @return The method `getPiece` is returning a `ChessPiece` object located
+     * at the specified `row` and `col` coordinates on the chess board.
+     */
     public ChessPiece getPiece(int row, int col) {
         return board[row][col];
     }
 
+    /**
+     * The method handles moving a chess piece on the board, capturing opponent
+     * pieces, checking for a winning condition, and updating the display
+     * accordingly.
+     *
+     * @param move The `move` array contains the starting row, starting column,
+     * ending row, and ending column of the move in a chess game.
+     * @return The method `movePiece` returns a boolean value. It returns `true`
+     * if the move was successfully executed, and it returns `false` if the move
+     * was invalid or not executed.
+     */
     public boolean movePiece(int[] move) {
         int startRow = move[0];
         int startCol = move[1];
         int endRow = move[2];
         int endCol = move[3];
-        
+
         ChessPiece selectedPiece = board[startRow][startCol];
         if (selectedPiece != null && isValidMove(startRow, startCol, endRow, endCol)) {
             ChessPiece targetPiece = board[endRow][endCol];
@@ -76,7 +112,9 @@ public class Chess {
 
     private boolean isValidMove(int startRow, int startCol, int endRow, int endCol) {
         ChessPiece piece = board[startRow][startCol];
-        if (piece == null) return false;
+        if (piece == null) {
+            return false;
+        }
         // Basic move validation logic based on the piece type (simplified here)
         // Implement specific rules for each piece type if needed
         return true;
